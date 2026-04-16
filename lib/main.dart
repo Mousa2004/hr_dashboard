5b4629cf867914618a67e98032341ccae4fcd520
Mohamed Ayman <mousa2692004@gmail.com>
2026-04-16 18:28:37 +0200

    Integration with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hr_dashboard/api/firebase_options.dart';
 import 'package:hr_dashboard/core/utils/app_routes.dart';
 import 'package:hr_dashboard/core/utils/app_theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
